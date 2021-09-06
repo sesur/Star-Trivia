a94b30b8db27f79e5de504e3f5bda4e94f1fac3b
sesur <sergiusuru@ymail.com>
2021-09-06 09:26:56 +0300

    Change project target name

---
+++ Star Trivia v0.3/StarTriviaUnitTests/StarshipViewControllerTests.swift
@@ -0,0 +1,39 @@
+//
+//  StarshipViewControllerTests.swift
+//  Star_TriviaUnitTests
+//
+//  Created by Sergiu on 06.09.2021.
+//  Copyright © 2021 Sergiu. All rights reserved.
+//
+
+import XCTest
+@testable import Star_Trivia
+
+class StarshipViewControllerTests: XCTestCase {
+    
+    func test_viewDidLoad() {
+     XCTAssertNotNil(makeSUT(), "viewDidLoad")
+    }
+    
+    func test_outlets_shouldBeConnected() {
+        XCTAssertNotNil(makeSUT().name, "name")
+        XCTAssertNotNil(makeSUT().model, "model")
+        XCTAssertNotNil(makeSUT().length, "lenght")
+        XCTAssertNotNil(makeSUT().maker, "maker")
+        XCTAssertNotNil(makeSUT().cost, "cost")
+        XCTAssertNotNil(makeSUT().speed, "speed")
+        XCTAssertNotNil(makeSUT().crew, "crew")
+        XCTAssertNotNil(makeSUT().passengers, "passengers")
+        XCTAssertNotNil(makeSUT().spinner, "spinner")
+    }
+    
+    //MARK:- Helpers
+    func makeSUT() -> StarshipViewController {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let sut: StarshipViewController = sb.instantiateViewController(identifier: String(describing: StarshipViewController.self))
+        
+        sut.loadViewIfNeeded()
+        
+        return sut
+    }
+}
